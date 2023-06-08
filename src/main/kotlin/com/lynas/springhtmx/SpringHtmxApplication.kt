39ac0fe94c4fcde57691cdb563a864acb18cb700
Sazzad Islam <sazzad.islam@reev.com>
2023-06-08 12:57:55 +0200

    form submit

--- src/main/kotlin/com/lynas/springhtmx/SpringHtmxApplication.kt
+++ src/main/kotlin/com/lynas/springhtmx/SpringHtmxApplication.kt
@@ -1,13 +1,19 @@
 package com.lynas.springhtmx
 
 import io.github.wimdeblauwe.hsbt.mvc.HtmxRequest
+import io.github.wimdeblauwe.hsbt.mvc.HxRefresh
 import io.github.wimdeblauwe.hsbt.mvc.HxRequest
+import io.github.wimdeblauwe.hsbt.mvc.HxTrigger
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestAttribute
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
@@ -23,7 +29,10 @@ fun main(args: Array<String>) {
 class IndexController {
 
 	@GetMapping("/")
-	fun index() = "index"
+	fun index(model: Model): String {
+		model.addAttribute("users", userList)
+		return "index"
+	}
 
 	@GetMapping("/users")
 	@HxRequest
@@ -60,3 +69,23 @@ var userList = mutableListOf(
 	User("Name1"),
 	User("Name2")
 )
+
+@RestController
+@RequestMapping("/users")
+class UserController{
+	@GetMapping
+	fun userList() = userList
+
+	@PostMapping("/validateName")
+	@HxRequest
+	fun validateUserName(@RequestParam name: String) : String {
+		return if (name.length < 3) "Invalid input" else ""
+	}
+
+	@PostMapping
+	@HxRequest
+	fun createNewUser(@ModelAttribute user: User) : String {
+		println(user)
+		return user.toString()
+	}
+}
